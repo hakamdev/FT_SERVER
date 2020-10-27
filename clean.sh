de6f1d8b3358b4fb1d75b1822f7adff0837272d7
Elkebir Hakam <ehakam@e2r11p6.1337.ma>
2020-10-27 18:48:29 +0100

    finished :D, yaay!

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
-sudo docker stop                $(sudo docker container ls -a -q) 000
-sudo docker container rm        $(sudo docker container ls -a -q) 000
-sudo docker image rm            $(sudo docker image ls -a -q) 000
-sudo docker image ls -a
-sudo docker container ls -a
+docker stop                $(docker container ls -a -q) 000
+docker container rm        $(docker container ls -a -q) 000
+docker image rm            $(docker image ls -a -q) 000
+docker image ls -a
+docker container ls -a
