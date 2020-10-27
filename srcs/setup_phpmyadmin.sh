a472576d8afadb660a907c63a2dbcebb03fd8565
Elkebir Hakam <ehakam@e2r11p6.1337.ma>
2020-10-27 18:07:32 +0100

    :)))))

--- srcs/setup_phpmyadmin.sh
+++ srcs/setup_phpmyadmin.sh
@@ -6,4 +6,6 @@ wget    https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-english.t
 tar     -xvf phpMyAdmin-5.0.2-english.tar.gz
 mv      phpMyAdmin-5.0.2-english /var/www/html/phpmyadmin
 rm      -f phpMyAdmin-5.0.2-english.tar.gz
+mkdir	/var/www/html/phpmyadmin/tmp
+chmod	777	/var/www/html/phpmyadmin/tmp
 echo    "=========================== End of PHPMYADMIN Configuration ============================="
--- srcs/start_all_services.sh
+++ srcs/start_all_services.sh
@@ -2,6 +2,7 @@
 #   Author: Ehakam      #
 #########################
 echo "Starting all services..."
+chown -R www-data:www-data /var/www/html/
 service mysql start
 service php7.3-fpm start
 service nginx start
