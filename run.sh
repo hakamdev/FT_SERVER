b7d13bbf3f5214b2ed9fd8d70eb970415ce28c56
Elkebir Hakam <ehakam@e2r11p6.1337.ma>
2020-10-27 17:35:24 +0100

    Solved VM issues! :)

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 clear
 echo "Initiating DOCKER BUILD of FT_SERVER image..."
-sudo docker build -t                    ft_server .
+docker build -t                    ft_server .
 echo "Running FT_SERVER container..."
-sudo docker run -p 80:80 -p 443:443 -it   ft_server
+docker run -p 80:80 -p 443:443 -it   ft_server
--- srcs/wp-config.php
+++ srcs/wp-config.php
@@ -37,6 +37,10 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define( 'WP_HOME', 'http://192.168.99.101/wordpress');
+
+define( 'WP_SITEURL', 'http://192.168.99.101/wordpress');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
