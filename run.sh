fe72d6709709c7e7c40686271cab7fd36fc73f04
Abd Elkebir HAKAM <abdouhakam8@gmail.com>
2020-12-22 19:23:22 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-clear
-echo "Initiating DOCKER BUILD of FT_SERVER image..."
-docker build -t                    ft_server .
-echo "Running FT_SERVER container..."
-docker run -p 80:80 -p 443:443 -it   ft_server
