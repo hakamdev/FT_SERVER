0358a462ce34366f0cef27f27d7a7929e7a60c94
Abd Elkebir HAKAM <abdouhakam8@gmail.com>
2020-12-22 19:23:31 +0100

    Delete clean.sh

--- clean.sh
+++
@@ -1,5 +0,0 @@
-docker stop                $(docker container ls -a -q) 000
-docker container rm        $(docker container ls -a -q) 000
-docker image rm            $(docker image ls -a -q) 000
-docker image ls -a
-docker container ls -a
